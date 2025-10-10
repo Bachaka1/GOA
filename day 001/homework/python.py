eb50fb191c2e86935ca040ab18664efcecd1fedd
Bachaka1 <mamukabachakashvili93@gmail.com>
2025-10-10 05:17:58 -0700

    second commit

---
+++ day 001/homework/python.py
@@ -0,0 +1,22 @@
+name = "mamuka"
+surname = "bachakashvili"
+age = 16
+height = 176
+print(name)
+print(surname)
+print(age)
+print(height)
+
+# ცვლადის დეკლარაცია ნიშნავს პროგრამაში ცვლადის შექმნას და გამოცხადებას, რათა მას შემდეგ პროგრამამ იცოდეს, რომ ეს ცვლადი არსებობს და შეუძლია მისთვის მეხსიერება გამოყოს.
+
+# print() - არის ფუნქცია, რომელსაც ეკრანზე გამოაქვს ინფორმაცია. იგი გამოიყენება იმისთვის, რომ პროგრამამ გვაჩვენოს: შედეგი, ტექსტი, რიცხვი, ცვლადის მნიშვნელობა და სხვა.
+
+name = "mamuka" #string - str()
+age = 16 # integer - int()
+weight = 66.1 # float - float()
+a = False # bool - bool()
+
+print( type(name) )
+print( type(age) )
+print( type(weight) )
+print( type(a) )
